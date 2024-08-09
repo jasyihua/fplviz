456ffddc7b7c9af9d1853f1084aa0adeb5c3bdee
jasyihua <jasyihua@gmail.com>
2024-08-09 15:24:49 +0100

    edit

--- main.py
+++ main.py
@@ -3,16 +3,17 @@ from pages import home, data_visualization, contact
 from components.sidebar import sidebar
 
 def main():
-    st.set_page_config(page_title="FPL Data Viz", page_icon="⚽", layout="wide")
+    st.set_page_config(page_title="FPL Data Viz", 
+                       page_icon="⚽", layout="wide")
     
-    page = sidebar()
+    # page = sidebar()
 
-    if page == "Home":
-        home.show()
-    elif page == "Data Visualization":
-        data_visualization.show()
-    elif page == "Contact":
-        contact.show()
+    # if page == "Home":
+    #     home.show()
+    # elif page == "Data Visualization":
+    #     data_visualization.show()
+    # elif page == "Contact":
+    #     contact.contact_form.show()
 
 if __name__ == "__main__":
     main()
